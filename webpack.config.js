b114b268a45ad6d65efb1546059e2cbdc3a46171
Frank Lam <frank@duckpun.ch>
2016-03-12 15:34:45 -0800

    really simple webpack

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index',
+    output: {
+        filename: 'app.js',
+        path: path.join(__dirname, './dist'),
+        publicPath: '/assets/'
+    }
+};
