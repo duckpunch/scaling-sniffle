c3e440cde548e20d90acf2be49101d6fe15720fe
Frank Lam <frank@duckpun.ch>
2016-03-12 16:37:18 -0800

    maybe the loader stanza isnt needed

--- src/index.js
+++ src/index.js
@@ -3,5 +3,3 @@ console.log('hi there again!');
 const roar = foo => console.log(foo);
 
 roar('blah');
-
-
--- webpack.config.js
+++ webpack.config.js
@@ -9,18 +9,5 @@ module.exports = {
         filename: 'app.js',
         path: path.join(__dirname, './dist'),
         publicPath: '/assets/'
-    },
-
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    presets: ['react', 'es2015']
-                }
-            }
-        ]
     }
 };
