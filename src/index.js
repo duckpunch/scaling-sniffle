46cf9b815b54750c9cff4e440b4ac0895003c021
Frank Lam <frank@duckpun.ch>
2016-03-12 17:48:11 -0800

    notes about state implementation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
+import {combineReducers, createStore} from 'redux';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
 
+const some_state = {
+    full_tree: [
+        {msg: 'hello'},
+        {msg: 'hello'},
+        {msg: 'hello'},
+        {msg: 'hello'},
+    ],
+    current_move: 0
+};
 
-const roar = foo => console.log(foo);
+function test1(state = 0, action) {
+    console.log(state);
+    return state;
+}
 
-roar('blah');
+function test2(state = 'roar', action) {
+    console.log(state);
+    return state;
+}
+
+//const reducers = combineReducers({
+    //test1, test2
+//});
+const store = createStore(test1);
+
+render(
+    <Provider store={store}>
+        <div></div>
+    </Provider>,
+    document.getElementById('app')
+);
